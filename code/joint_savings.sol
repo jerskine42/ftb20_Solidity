71748fb636bc767f459a188d2a81a45c7b51ffc4
jerskine42 <110424813+jerskine42@users.noreply.github.com>
2023-01-30 18:53:18 -0500

    reran Step 3 and updated  ReadMe.md

--- code/joint_savings.sol
+++ code/joint_savings.sol
@@ -2,14 +2,17 @@
 Joint Savings Account
 ---------------------
 
-To automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses that are then able to control a joint savings account. 
-Your smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
+To automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses 
+that are then able to control a joint savings account. Your smart contract will use ether management functions to implement 
+various requirements from the financial institution to provide the features of the joint savings account.
 
 The Starting file provided for this challenge contains a `pragma` for solidity version `5.0.0`.
 You will do the following:
 1. Create and work within a local blockchain development environment using the JavaScript VM provided by the Remix IDE.
 2. Script and deploy a **JointSavings** smart contract.
 3. Interact with your deployed smart contract to transfer and withdraw funds.
+    - Dummy account1 address: 0x0c0669Cd5e60a6F4b8ce437E4a4A007093D368Cb
+    - Dummy account2 address: 0x7A1f3dFAa0a4a19844B606CD6e91d693083B12c0
 
 */
 
@@ -57,7 +60,7 @@ contract JointSavings {
         lastWithdrawAmount = amount;
 
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
-        contractBalance = address(this).balance - amount;
+        contractBalance = address(this).balance;
   
     }
 
